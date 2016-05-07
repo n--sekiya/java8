5aca50fdb439e2fa0475fc647b7a2a3be1f5472c
n--sekiya <n_sekiya@atlas.jp>
2016-05-07 11:39:52 +0900

    Initial commit
    テストコミット

---
+++ src/model/MobileSuitModel.java
@@ -0,0 +1,28 @@
+package model;
+
+
+/**
+ * 機体モデル
+ * @author n_sekiya
+ */
+public class MobileSuitModel {
+
+	/** 型番 */
+	private String number;
+	/** 名称 */
+	private String name;
+
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
---
+++ src/model/PilotModel.java
@@ -0,0 +1,50 @@
+package model;
+
+
+/**
+ * 機体モデル
+ * @author n_sekiya
+ */
+public class PilotModel {
+//	/** 性 */
+//	private String family;
+//	/** ミドルネーム */
+//	private String middle;
+//	/** 名 */
+//	private String last;
+	/** 名前 */
+	private String name;
+	/** 性別 */
+	private int gender;
+
+//	public String getFamily() {
+//		return family;
+//	}
+//	public void setFamily(String family) {
+//		this.family = family;
+//	}
+//	public String getMiddle() {
+//		return middle;
+//	}
+//	public void setMiddle(String middle) {
+//		this.middle = middle;
+//	}
+//	public String getLast() {
+//		return last;
+//	}
+//	public void setLast(String last) {
+//		this.last = last;
+//	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getGender() {
+		return gender;
+	}
+	public void setGender(int gender) {
+		this.gender = gender;
+	}
+}
