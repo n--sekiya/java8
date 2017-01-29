e5711767fff76e65723352d0ba7a36a78c713dc7
n--sekiya <n_sekiya@atlas.jp>
2017-01-29 14:33:47 +0900

    joinerのサンプル追加

--- src/java8/logic/OptionalSample.java
+++ src/java8/logic/OptionalSample.java
@@ -3,19 +3,19 @@ package java8.logic;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.StringJoiner;
 
 public class OptionalSample {
 
-	
-	
-	public static void main() {
-		final int value = 1;
+	public static void main(String[] args) {
+		final int value = 2;
 		System.out.println("---START---");
 		switch(value) {
 			case 1:
 				sample1();
 				break;
 			case 2:
+				sample2();
 				break;
 			case 3:
 				break;
@@ -24,6 +24,7 @@ public class OptionalSample {
 		}
 		System.out.println("---END---");
 	}
+
 	private static void sample1() {
 		String value = "俺がガンダムだ";
 		Optional<String> optional = Optional.ofNullable(value);
@@ -36,6 +37,17 @@ public class OptionalSample {
 		optional.ifPresent(v -> list.add(v));
 		optional.ifPresent(list::add);
 		System.out.println(list.size());
+	}
+
+	private static void sample2() {
+		final StringJoiner joiner = new StringJoiner(",", "(", ")").setEmptyValue("");;
+		final String value = "ガンプラは自由だ";
+		final Optional<String> optional = Optional.ofNullable(value);
+	
+		// 値が存在する時のみ実行される
+		// ※nullだと実行されないがから文字だと実行される
+		optional.ifPresent(joiner::add);
+		System.out.println(joiner.toString());
 		
 	}
 
